1445ce00e751989f7c70d4dc8de8c3034cbb305c
nishad2134 <158585647+nishad2134@users.noreply.github.com>
2024-06-14 01:03:37 +0530

    SQL task 1

---
+++ 1task_40rows.sql
@@ -0,0 +1,54 @@
+CREATE TABLE employees (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    salary DECIMAL(10, 2)
+);
+select * from employees
+
+INSERT INTO employees VALUES 
+(1, 'Employee 1', 30, 50000),
+(2, 'Employee 2', 31, 51000),
+(3, 'Employee 3', 32, 52000),
+(4, 'Employee 4', 33, 53000),
+(5, 'Employee 5', 34, 54000),
+(6, 'Employee 6', 35, 55000),
+(7, 'Employee 7', 36, 56000),
+(8, 'Employee 8', 37, 57000),
+(9, 'Employee 9', 38, 58000),
+(10, 'Employee 10', 39, 59000),
+(11, 'Employee 11', 40, 60000),
+(12, 'Employee 12', 41, 61000),
+(13, 'Employee 13', 42, 62000),
+(14, 'Employee 14', 43, 63000),
+(15, 'Employee 15', 44, 64000),
+(16, 'Employee 16', 45, 65000),
+(17, 'Employee 17', 46, 66000),
+(18, 'Employee 18', 47, 67000),
+(19, 'Employee 19', 48, 68000),
+(20, 'Employee 20', 49, 69000),
+(21, 'Employee 21', 50, 70000),
+(22, 'Employee 22', 51, 71000),
+(23, 'Employee 23', 52, 72000),
+(24, 'Employee 24', 53, 73000),
+(25, 'Employee 25', 54, 74000),
+(26, 'Employee 26', 55, 75000),
+(27, 'Employee 27', 56, 76000),
+(28, 'Employee 28', 57, 77000),
+(29, 'Employee 29', 58, 78000),
+(30, 'Employee 30', 59, 79000),
+(31, 'Employee 31', 60, 80000),
+(32, 'Employee 32', 61, 81000),
+(33, 'Employee 33', 62, 82000),
+(34, 'Employee 34', 63, 83000),
+(35, 'Employee 35', 64, 84000),
+(36, 'Employee 36', 65, 85000),
+(37, 'Employee 37', 66, 86000),
+(38, 'Employee 38', 67, 87000),
+(39, 'Employee 39', 68, 88000),
+(40, 'Employee 40', 69, 89000);
+select * from employees
+
+
+
+
